e6ecc3f3fd3a65f8459c1961e9315403d28dc577
sergiopaezdev <srpaez@gmail.com>
2022-05-18 17:36:01 -0300

    first commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -4,7 +4,7 @@ function obtenerMayor(x, y) {
   // "x" e "y" son números enteros (int).
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
-  // Tu código:sssssssss
+  // Tu código:
 }
 
 function mayoriaDeEdad(edad) {
